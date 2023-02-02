b7863ef6435a6dea33dfdd821e94ea14ee4d43b8
Jerick-CM <jmangaluz@gmail.com>
2023-02-02 14:10:25 +0800

    fix directory route

--- src/Providers/TestProvider.php
+++ src/Providers/TestProvider.php
@@ -17,7 +17,7 @@ class TestProvider extends ServiceProvider
 
         // $this->loadMigrationsFrom(__DIR__.'../../database/migrations');
 	    $this->loadRoutesFrom(__DIR__ . '/../Routes/web.php');
-        $this->loadMigrationsFrom(__DIR__ . '../../../database/migrations');
+        $this->loadMigrationsFrom(__DIR__ . '/../../database/migrations');
 
         // if ($this->app->runningInConsole()) {
         //     if (!class_exists('CreatePostsTable')) {
