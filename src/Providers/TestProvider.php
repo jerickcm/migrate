5dd0d42f0d16897c15f54f8f224d10edf07b960b
Jerick-CM <jmangaluz@gmail.com>
2023-02-02 14:53:44 +0800

    fix

--- src/Providers/TestProvider.php
+++ src/Providers/TestProvider.php
@@ -15,7 +15,7 @@ class TestProvider extends ServiceProvider
         // $this->loadMigrationsFrom(__DIR__.'../../database/migrations');
 	    $this->loadRoutesFrom(__DIR__ . '/../Routes/web.php');
         // $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
-        $this->loadMigrationsFrom(base_path() . '/vendor/jerickcm/migrate/database/migration');
+        $this->loadMigrationsFrom(base_path() . '/vendor/jerickcm/migrate/src/database/migration');
 
         // if ($this->app->runningInConsole()) {
         //     if (!class_exists('CreatePostsTable')) {
