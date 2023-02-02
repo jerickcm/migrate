45ed02ec82309a441e40ff9fb3673ccc93c2701b
Jerick-CM <jmangaluz@gmail.com>
2023-02-02 13:28:12 +0800

    test package migration 2

--- src/Providers/TestProvider.php
+++ src/Providers/TestProvider.php
@@ -12,14 +12,15 @@ class TestProvider extends ServiceProvider
     {
         // $this->loadMigrationsFrom(__DIR__.'../../database/migrations');
 	    $this->loadRoutesFrom(__DIR__ . '/../Routes/web.php');
+        $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
 
-        if ($this->app->runningInConsole()) {
-            if (!class_exists('CreatePostsTable')) {
-                $this->publishes([
-                    __DIR__ . '/../database/migrations/create_posts_table.php.stub' => database_path('migrations/' . date('Y_m_d_His', time()) . '_create_posts_table.php'),
-                ], 'migrations');
-            }
-        }
+        // if ($this->app->runningInConsole()) {
+        //     if (!class_exists('CreatePostsTable')) {
+        //         $this->publishes([
+        //             __DIR__ . '/../database/migrations/create_posts_table.php.stub' => database_path('migrations/' . date('Y_m_d_His', time()) . '_create_posts_table.php'),
+        //         ], 'migrations');
+        //     }
+        // }
 
         // $this->loadViewsFrom(__DIR__.'/../views', 'inspire');
     }
