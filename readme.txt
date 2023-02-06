191cdb3facd73bd6be1b56f8582631513dc92306
Jerick-CM <jmangaluz@gmail.com>
2023-02-06 14:23:56 +0800

    fix migration url

--- readme.txt
+++ readme.txt
@@ -1,1 +1,3 @@
 php artisan vendor:publish --provider="Jerickcm\Migrate\TestProvider" --tag="migrations"
+
+ php artisan vendor:publish --provider="Jerickcm\Migrate\Providers\TestProvider" --tag="migrations"
--- src/Providers/TestProvider.php
+++ src/Providers/TestProvider.php
@@ -21,7 +21,7 @@ class TestProvider extends ServiceProvider
         if ($this->app->runningInConsole()) {
             if (!class_exists('CreatePostsTable')) {
                 $this->publishes([
-                    base_path() . '/vendor/jerickcm/migrate/stub/create_posts_table.php.stub' => database_path('migrations/' . date('Y_m_d_His', time()) . '_create_posts_table.php'),
+                    base_path() . '/vendor/jerickcm/migrate/database/stub/create_posts_table.php.stub' => database_path('migrations/' . date('Y_m_d_His', time()) . '_create_posts_table.php'),
                 ], 'migrations');
             }
         }
