5db82d8172d7284ce200247200dc1a89fab5052f
Jerick-CM <jmangaluz@gmail.com>
2023-02-06 10:46:05 +0800

    test stub

--- src/Providers/TestProvider.php
+++ src/Providers/TestProvider.php
@@ -11,27 +11,24 @@ class TestProvider extends ServiceProvider
     public function boot()
     {
 
-
         // $this->loadMigrationsFrom(__DIR__.'../../database/migrations');
-	    $this->loadRoutesFrom(__DIR__ . '/../Routes/web.php');
         // $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
-        $this->loadMigrationsFrom(base_path() . '/vendor/jerickcm/migrate/database/migration');
-
-        // if ($this->app->runningInConsole()) {
-        //     if (!class_exists('CreatePostsTable')) {
-        //         $this->publishes([
-        //             __DIR__ . '/../database/migrations/create_posts_table.php.stub' => database_path('migrations/' . date('Y_m_d_His', time()) . '_create_posts_table.php'),
-        //         ], 'migrations');
-        //     }
-        // }
-
+        // $this->loadMigrationsFrom(base_path() . '/vendor/jerickcm/migrate/database/migration');
         // $this->loadViewsFrom(__DIR__.'/../views', 'inspire');
 
+        $this->loadRoutesFrom(__DIR__ . '/../Routes/web.php');
+
+        if ($this->app->runningInConsole()) {
+            if (!class_exists('CreatePostsTable')) {
+                $this->publishes([
+                    base_path() . '/vendor/jerickcm/migrate/stub/create_posts_table.php.stub' => database_path('migrations/' . date('Y_m_d_His', time()) . '_create_posts_table.php'),
+                ], 'migrations');
+            }
+        }
     }
 
     public function register()
     {
-
     }
 
 }
